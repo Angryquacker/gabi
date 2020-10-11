4ef3cdec1cfe797a8e1bca089dd4dde4d18a1f87
Travis Hahn <tjxhahn@gmail.com>
2020-10-11 00:32:59 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,35 @@
+const moreIns = ins => {
+  fetch("https://type.fit/api/quotes")
+  .then(res => res.json())
+  .then(data => { 
+    let quote = data[Math.floor(Math.random() * 1000)];
+    ins.innerHTML = `"${quote.text}" - ${quote.author}`;
+  });
+};
+
+const moreHapp = img => {
+  fetch("https://api.thecatapi.com/v1/images/search?size=full")
+  .then(res => res.json())
+  .then(data => {
+    img.src = data[0].url;
+  });
+};
+
+window.onload = () => {
+  let ins = document.getElementById("quote");
+  let img = document.getElementById("pic");
+  
+  document.getElementById("mI").addEventListener("click", e => {
+    e.preventDefault();
+    moreIns(ins);
+  });
+  
+  document.getElementById("mH").addEventListener("click", e => {
+    e.preventDefault();
+    moreHapp(img);
+  });
+  
+  moreIns(ins);
+  moreHapp(img);
+}
+
